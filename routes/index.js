a72d1b66d6b655feee76577b35430d74c7cc2bc4
adityain98 <adityain98@gmail.com>
2019-11-01 14:53:58 +0700

    selesai

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const userRoute = require('./userRoute');
 const zomatoRoute = require('./zomatoRoute')
 const directionRoute = require('./directionRoute')
 const favRoute = require('./favRoute');
+const yesNoRoute = require('./yesNoRoute')
 
 Route.use('/', userRoute);
 Route.use('/directions', directionRoute)
 Route.use('/zomato', zomatoRoute)
 Route.use('/fav', favRoute);
+Route.use('/yesno', yesNoRoute)
 
 module.exports = Route;
