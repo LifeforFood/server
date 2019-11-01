5638d3035af32062841d672f52159f2a0b68c366
dadepe <rizky_dwifutra@yahoo.com>
2019-11-01 10:15:49 +0700

    baru

--- routes/zomatoRoute.js
+++ routes/zomatoRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const ZomatoController = require('../controllers/zomatoController')
 
 router.get('/restaurants/:cityId', ZomatoController.getRestaurants)
+router.get('/restaurants/:cityId/search?', ZomatoController.searchRestaurants)
 router.get('/random/:cityId', ZomatoController.randomRestaurant)
 router.get('/:restaurantId', ZomatoController.restaurantDetail)
 
